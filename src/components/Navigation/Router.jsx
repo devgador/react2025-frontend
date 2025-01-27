4d8fa44bf8622e050316ad88da5f1d421b78ff72
mijaso <lbm90210@gmail.org>
2025-01-26 23:11:15 -0400

    FINAL

--- src/components/Navigation/Router.jsx
+++ src/components/Navigation/Router.jsx
@@ -3,11 +3,14 @@ import About from "../../screens/About.jsx";
 import Product from "../../screens/Product.jsx";
 import Think from "../../screens/Think.jsx";
 import Counter from "../Counter/Counter.jsx";
+import Login from "../../screens/Login.jsx";
 import { Routes, Route } from "react-router";
+import LandingPage from "../../screens/LandingPage.jsx";
 const Router = () => {
     return (
         <Routes>
-            <Route index element={<Home />} />
+            <Route index element={<LandingPage />} />
+            <Route path="login" element={<Login />} />
             <Route path="product" element={<Product />} />
             <Route path="think" element={<Think />} />
             <Route path="about" element={<About />} />
--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './features/counter/counterSlice'
 import productReducer from './features/product/productSlice.js'
+import formReducer from './form/formSlice.js'
 
 const store = configureStore({
     reducer: {
         counter: counterReducer,
         productr: productReducer,
+        formRed: formReducer,
     },
 })
 
